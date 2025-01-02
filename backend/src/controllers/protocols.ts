7279a8638cfbbf5cebfc271c139bb4aa6d6a03da
Juliowk <julioeliasdesousarocha.work@gmail.com>
2025-01-02 11:09:55 -0300

    feat: Definition of controller protocols

---
+++ backend/src/controllers/protocols.ts
@@ -0,0 +1,21 @@
+export enum HttpStatusCode {
+  OK = 200,
+  CREATED = 201,
+  BAD_REQUEST = 400,
+  SERVER_ERROR = 500,
+}
+
+export interface iController {
+  handle(httpRequest: HttpRequest<unknown>): Promise<HttpResponse<unknown>>;
+}
+
+export interface HttpResponse<T> {
+  statusCode: HttpStatusCode;
+  body: T;
+}
+
+export interface HttpRequest<T> {
+  body?: T;
+  paramas?: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+  headers?: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+}
