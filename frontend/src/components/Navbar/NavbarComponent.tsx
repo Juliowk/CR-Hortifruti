d7353ddeed892cd2ddfaf1b58facbd225ec1ba02
Juliowk <julioeliasdesousarocha.work@gmail.com>
2025-01-05 13:39:59 -0300

    refactor: Adding icons

--- frontend/src/components/Navbar/NavbarComponent.tsx
+++ frontend/src/components/Navbar/NavbarComponent.tsx
@@ -3,12 +3,14 @@ import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import Offcanvas from "react-bootstrap/Offcanvas";
 
 import logo from "../../images/3-removebg-preview.png";
 import styles from "./navbar.module.css";
 import { useEffect, useState } from "react";
+import { FaGithub, FaWhatsapp } from "react-icons/fa";
+import { LuLogIn } from "react-icons/lu";
+import { MdOutlineSearch } from "react-icons/md";
 
 function Navbar_Component() {
   const [isCollapse, setIsCollapse] = useState(false);
@@ -73,23 +75,21 @@ function Navbar_Component() {
                 className="me-2"
                 aria-label="Search"
               />
-              <Button variant="outline-secondary">Search</Button>
+              <Button variant="outline-secondary">
+                <MdOutlineSearch size={30} />
+              </Button>
             </Form>
 
-            <Nav className="justify-content-end flex-grow-1 pe-3">
-              <Nav.Link href="#">Home</Nav.Link>
-              <Nav.Link href="#">Link</Nav.Link>
-              <NavDropdown
-                title="Dropdown"
-                id={`offcanvasNavbarDropdown-expand-sm`}
-              >
-                <NavDropdown.Item href="#">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#">Another action</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#">
-                  Something else here
-                </NavDropdown.Item>
-              </NavDropdown>
+            <Nav className={`justify-content-end flex-grow-1 pe-3`}>
+              <Nav.Link href="#" className={isCollapse ? "mb-3 mt-3" : ""}>
+                <FaWhatsapp size={30} /> {isCollapse && "Whatsapp"}
+              </Nav.Link>
+              <Nav.Link href="#" className={isCollapse ? "mb-3" : ""}>
+                <FaGithub size={29} /> {isCollapse && "GitHub"}
+              </Nav.Link>
+              <Nav.Link href="#" className={isCollapse ? "mb-3" : ""}>
+                <LuLogIn size={29} /> {isCollapse && "Login"}
+              </Nav.Link>
             </Nav>
           </Offcanvas.Body>
         </Navbar.Offcanvas>
