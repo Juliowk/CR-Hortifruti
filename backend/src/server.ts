61c46cef5eac82906216c765c9fbc7cd2ad2380f
Juliowk <julioeliasdesousarocha.work@gmail.com>
2025-01-05 15:21:51 -0300

    feat: Backend integration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,20 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import { MongoConnect } from "./database/Mongo.js";
 import routerProducts from "./routes/products.js";
 
 const main = async () => {
   dotenv.config();
-
+  
   const PORT = process.env.PORT || "4000";
-
+  
   await MongoConnect.connect();
-
+  
   const app = express();
   app.use(express.json());
+  app.use(cors());
 
-  app.use("/products", routerProducts)
+  app.use("/products", routerProducts);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
 import Carousel_Component from "./components/Carrousel/CarouselComponent";
 import Navbar_Component from "./components/Navbar/NavbarComponent";
 import Products_Grid_Component from "./components/Products/ProductsGridComponent";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/products")
+      .then((result) => {
+        if (!result.ok) throw new Error("Request error");
+        return result.json();
+      })
+      .then((json) => {
+        setProducts(json);
+      })
+      .catch((error) => {
+        console.error(`Erro: ${error}`);
+      });
+  }, []);
+
+  console.log(products);  
+
   return (
     <>
       <Navbar_Component />
