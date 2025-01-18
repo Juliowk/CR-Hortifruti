80a63fc912264e67888af6743b21c675371715ad
Juliowk <julioeliasdesousarocha.work@gmail.com>
2025-01-18 09:33:30 -0300

    refactor: Correction of the return type of the update method

--- backend/src/controllers/Products/Get/getProducts.ts
+++ backend/src/controllers/Products/Get/getProducts.ts
@@ -1,10 +1,11 @@
+import Product from "../../../models/Product.js";
 import { HttpResponse, HttpStatusCode, IController } from "../../protocols.js";
 import { IGetProductsRepository } from "./protocols.js";
 
 export class GetProductsController implements IController {
   constructor(private readonly repository: IGetProductsRepository) {}
 
-  async handle(): Promise<HttpResponse<unknown | string>> {
+  async handle(): Promise<HttpResponse<Product[] | string>> {
     try {
       const users = await this.repository.getProducts();
       return {
